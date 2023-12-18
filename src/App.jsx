d684efdc78fb8c347a29b7e90ef7483afb30859b
buddhathemanoj <prabakarmanoj743@gmail.com>
2023-12-19 00:03:33 +0530

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home/Home";
